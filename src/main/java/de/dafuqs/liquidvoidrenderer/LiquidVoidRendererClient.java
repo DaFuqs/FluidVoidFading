cdf7f6cdcda63f77117ea53c689d2ab3bf8ff4a3
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-02-02 09:59:19 +0100

    making flowing lava translucent

--- src/main/java/de/dafuqs/liquidvoidrenderer/LiquidVoidRendererClient.java
+++ src/main/java/de/dafuqs/liquidvoidrenderer/LiquidVoidRendererClient.java
@@ -3,12 +3,16 @@ package de.dafuqs.liquidvoidrenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.*;
+import net.minecraft.client.render.*;
+import net.minecraft.fluid.*;
 
 @Environment(EnvType.CLIENT)
 public class LiquidVoidRendererClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putFluid(Fluids.FLOWING_LAVA, RenderLayer.getTranslucent());
     }
 
 }
