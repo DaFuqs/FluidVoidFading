07c1d6bebc8cfbf4b4f30952890c57a01018938c
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-04-26 16:23:26 +0200

    Sodium 0.8.7 compat. Fixes #22 & fixes #23

--- src/main/java/de/dafuqs/fluidvoidfading/mixin/client/SodiumDefaultFluidRendererMixin.java
+++ src/main/java/de/dafuqs/fluidvoidfading/mixin/client/SodiumDefaultFluidRendererMixin.java
@@ -158,10 +158,11 @@ public abstract class SodiumDefaultFluidRendererMixin {
         }
     }
 
-    @Inject(method = "isSideExposed", at = @At("HEAD"), cancellable = true)
-    private void fluidVoidFading$isSideExposed(BlockAndTintGetter world, int x, int y, int z, Direction dir, float height, CallbackInfoReturnable<Boolean> cir) {
-        if (dir == Direction.DOWN && y == world.getMinY())
+    @Inject(method = "isSideExposedOffset", at = @At("HEAD"), cancellable = true)
+    private void fluidVoidFading$isSideExposed(BlockAndTintGetter world, BlockState ownBlockState, BlockPos originPos, Direction dir, float height, CallbackInfoReturnable<Boolean> cir) {
+        if (dir == Direction.DOWN && originPos.getY() == world.getMinY()) {
             cir.setReturnValue(false);
+        }
     }
 
     @Unique
